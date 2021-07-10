db95f64ff16189b802a52fb82f61333af64bb791
Md Ahsanul Hoque <ahsan90@users.noreply.github.com>
2021-07-10 16:47:55 -0300

    Setup server running

--- api/server.js
+++ api/server.js
@@ -1,4 +1,10 @@
-const express = require('express');
-import bodyParser from 'body-parser';
+import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
+const app = express();
+
+app.get('/', (req, res) => res.send("Hello world"));
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`The server is running on ${PORT}`))
